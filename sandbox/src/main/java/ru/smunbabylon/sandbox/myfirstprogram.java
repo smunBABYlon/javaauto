99896a5c996b6f29599ee55a29df8ffaa14ddd62
Aleksandr Beliaev <beliaev.aleksandr@haulmont.com>
2021-04-29 10:33:14 +0400

    Обновление форматирования

--- sandbox/src/main/java/ru/smunbabylon/sandbox/myfirstprogram.java
+++ sandbox/src/main/java/ru/smunbabylon/sandbox/myfirstprogram.java
@@ -2,8 +2,7 @@ package ru.smunbabylon.sandbox;
 
 public class myfirstprogram {
 
-	public static void main(String[] args)
-	{
-System.out.println("Hello, world");
-	}
+  public static void main(String[] args) {
+    System.out.println("Hello, world");
+  }
 }
