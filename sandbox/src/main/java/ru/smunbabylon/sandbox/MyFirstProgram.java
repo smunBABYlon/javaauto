5b39fbbe79a3914cb3d7f8465b5c191175b63e91
Aleksandr Beliaev <beliaev.aleksandr@haulmont.com>
2021-04-29 17:15:15 +0400

    Создан класс точки и статичная функция вычисления расстояния

--- sandbox/src/main/java/ru/smunbabylon/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/smunbabylon/sandbox/MyFirstProgram.java
@@ -13,11 +13,22 @@ public class MyFirstProgram {
     Rectangle r = new Rectangle(6,7);
     System.out.println("Площадь квадрата со стороной " + s.l + " равна " + s.area());
     System.out.println("Площадь прямоугольника " + "равна " + r.area());
+
+    Point a = new Point(-2,-5);
+    Point b = new Point(1,-1);
+    System.out.println(distance(a,b));
   }
 
   public static String hello(String somebody, String something, int HowMuch){
     //System.out.println("Дайте " + somebody + " " + HowMuch + " " + something);
     return "Дайте " + somebody + " " + HowMuch;
+  }
+
+  public static double distance(Point p1, Point p2){
+    double xx = p1.x - p2.x;
+    double yy = p1.y - p2.y;
+    double s = xx * xx + yy * yy;
+    return Math.sqrt(s);
   }
 
 
