3a9483999dfaabdea7436990b16927a9a4830e8e
Aleksandr Beliaev <beliaev.aleksandr@haulmont.com>
2021-05-04 13:18:46 +0400

    Рефакторинг

---
+++ addressbook-web-tests/src/test/java/ru/smunbabylon/addressbook/ApplicationManager.java
@@ -0,0 +1,100 @@
+package ru.smunbabylon.addressbook;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class ApplicationManager {
+  private WebDriver driver;
+
+  protected void init() {
+    driver = new ChromeDriver();
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    login("admin", "secret");
+  }
+
+  private void login(String username, String password) {
+    driver.get("http://localhost/addressbook/");
+    driver.findElement(By.name("user")).click();
+    driver.findElement(By.name("user")).clear();
+    driver.findElement(By.name("user")).sendKeys(username);
+    driver.findElement(By.name("pass")).clear();
+    driver.findElement(By.name("pass")).sendKeys(password);
+    driver.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+
+  protected void returnToGroupPage() {
+    driver.findElement(By.linkText("group page")).click();
+  }
+
+  protected void submitGroupCreation() {
+    driver.findElement(By.name("submit")).click();
+  }
+
+  protected void fillGroupForm(GroupData groupData) {
+    driver.findElement(By.name("group_name")).click();
+    driver.findElement(By.name("group_name")).clear();
+    driver.findElement(By.name("group_name")).sendKeys(groupData.getName());
+    driver.findElement(By.name("group_header")).click();
+    driver.findElement(By.name("group_header")).clear();
+    driver.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
+    driver.findElement(By.name("group_footer")).click();
+    driver.findElement(By.name("group_footer")).clear();
+    driver.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+  }
+
+  protected void initGroupCreation() {
+    driver.findElement(By.name("new")).click();
+  }
+
+  protected void goToGroupPage() {
+    driver.findElement(By.linkText("groups")).click();
+  }
+
+  protected void stop() {
+    driver.quit();
+  }
+
+  protected void deleteSelectedGroups() {
+    driver.findElement(By.name("delete")).click();
+  }
+
+  protected void selectGroup() {
+    driver.findElement(By.name("selected[]")).click();
+  }
+
+  protected void goToHomePage() {
+    driver.findElement(By.linkText("home page")).click();
+  }
+
+  protected void submitUserForm() {
+    driver.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+  }
+
+  protected void fillUserForm(UserData userData) {
+    driver.findElement(By.name("firstname")).click();
+    driver.findElement(By.name("firstname")).clear();
+    driver.findElement(By.name("firstname")).sendKeys(userData.getName());
+    driver.findElement(By.name("lastname")).click();
+    driver.findElement(By.name("lastname")).clear();
+    driver.findElement(By.name("lastname")).sendKeys(userData.getLastname());
+    driver.findElement(By.name("nickname")).click();
+    driver.findElement(By.name("nickname")).clear();
+    driver.findElement(By.name("nickname")).sendKeys(userData.getNick());
+    driver.findElement(By.name("address")).click();
+    driver.findElement(By.name("address")).clear();
+    driver.findElement(By.name("address")).sendKeys(userData.getAddress());
+    driver.findElement(By.name("home")).click();
+    driver.findElement(By.name("home")).clear();
+    driver.findElement(By.name("home")).sendKeys(userData.getHomePhone());
+    driver.findElement(By.name("email")).click();
+    driver.findElement(By.name("email")).clear();
+    driver.findElement(By.name("email")).sendKeys(userData.getEmail());
+  }
+
+  protected void goToUserCreationPage() {
+    driver.findElement(By.linkText("add new")).click();
+  }
+}
