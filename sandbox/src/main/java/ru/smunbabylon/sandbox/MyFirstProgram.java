0583a1d432a9230cb2c44ac15426b6c607227a13
Aleksandr Beliaev <beliaev.aleksandr@haulmont.com>
2021-05-04 10:23:38 +0400

    Создан проект адрессбук автотест

--- sandbox/src/main/java/ru/smunbabylon/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/smunbabylon/sandbox/MyFirstProgram.java
@@ -1,15 +1,18 @@
 package ru.smunbabylon.sandbox;
 
+import java.util.Scanner;
+
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world");
     hello("кисоньке", "маракуйи", 4);
     hello("мне", "яблока", 2);
     String proba = hello("kate", "Apples", 8);
     System.out.println(proba);
     System.out.println(hello("kate", "Apples", 5));
-    Square s = new Square(5);
+    System.out.println("введите сторону квадрата");
+    Scanner squareSide = new Scanner(System.in);
+    Square s = new Square(squareSide.nextDouble());
     Rectangle r = new Rectangle(6,7);
     System.out.println("Площадь квадрата со стороной " + s.l + " равна " + s.area());
     System.out.println("Площадь прямоугольника " + "равна " + r.area());
