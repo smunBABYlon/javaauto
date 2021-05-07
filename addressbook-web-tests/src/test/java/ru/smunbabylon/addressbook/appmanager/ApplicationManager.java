d536de613afa0a30f35ba32bffa5d3b4fdc70205
Aleksandr Beliaev <beliaev.aleksandr@haulmont.com>
2021-05-07 23:57:13 +0400

    Добавлен код для выбора браузера. Добавлены оптимизации для незаполнения текстового поля пустым значением и незаполнения значением, уже существующим в поле

--- addressbook-web-tests/src/test/java/ru/smunbabylon/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/smunbabylon/addressbook/appmanager/ApplicationManager.java
@@ -2,6 +2,10 @@ package ru.smunbabylon.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.GeckoDriverService;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -12,9 +16,21 @@ public class ApplicationManager {
   private NavigationHelper navigationHelper;
   private SessionHelper sessionHelper;
   private GroupHelper groupHelper;
+  private String browser;
+
+  public ApplicationManager(String browser) {
+
+    this.browser = browser;
+  }
 
   public void init() {
-    driver = new ChromeDriver();
+    if (browser.equals(BrowserType.FIREFOX)) {
+      driver = new FirefoxDriver();
+    } else if (browser.equals(BrowserType.CHROME)){
+      driver = new ChromeDriver();
+    } else if (browser.equals(BrowserType.EDGE)) {
+      driver = new EdgeDriver();
+    }
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     groupHelper = new GroupHelper(driver);
     userHelper = new UserHelper(driver);
